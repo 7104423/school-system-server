1271bdd10ecf42ebe40443d2e8db2b824dfd0136
Jan Janás <Jan.Janas@UnicornUniversity.net>
2022-02-23 14:43:42 +0100

    server initialization

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+
+// MIDDLEWARE
+
+app.use(express.json());
+
+
+// ROUTES
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
