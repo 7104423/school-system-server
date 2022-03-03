872a3c40cb0abd663baa74e126dd651ce3b38d0b
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2022-03-03 23:59:31 +0100

    [ADD]  file

--- utils/session.js
+++ utils/session.js
@@ -1,3 +1,7 @@
+/*
+ * For more info see: https://medium.com/@prashantramnyc/node-js-with-passport-authentication-simplified-76ca65ee91e5
+ */
+
 const checkAuthenticated = (req, res, next) => {
   if (req.isAuthenticated()) {
     return next();
@@ -5,4 +9,19 @@ const checkAuthenticated = (req, res, next) => {
   return res.redirect('/login');
 };
 
-module.exports = [checkAuthenticated];
+const checkLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return res.redirect('/dashboard');
+  }
+  return next();
+};
+
+const logOut = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+  req.logOut();
+  return res.redirect('/login');
+};
+
+module.exports = [checkAuthenticated, checkLoggedIn, logOut];
