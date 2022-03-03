92bed215d2d276a71eb3bdeb04d238ed4f636887
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2022-03-03 23:37:57 +0100

    [ADD] Express generator init

--- app.js
+++ app.js
@@ -1,22 +1,44 @@
-// akletip first commit
-const express = require("express");
+// Imports
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const passport = require('passport');
+const session = require('express-session');
+const LocalStrategy = require('passport-local').Strategy;
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
+// App config
 const app = express();
-const port = process.env.PORT || 3001;
-
-
-// MIDDLEWARE
-
+app.use(logger('dev'));
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-
-// ROUTES
-
-app.get("/", (req, res) => {
-    res.send("Hello World");
+// Auth
+const authUser = (user, password, done) => {
+  const authenticatedUser = { id: 123, name: 'Kyle' };
+  return done(null, authenticatedUser);
+};
+passport.use(new LocalStrategy(authUser));
+passport.serializeUser((userObj, done) => {
+  done(null, userObj);
+});
+passport.deserializeUser((userObj, done) => {
+  done(null, userObj);
 });
 
+// Router
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
+module.exports = app;
