f303230baeca8e2e64f5ed34f65c5fb604b909ce
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2022-03-04 02:56:10 +0100

    [EDIT] Change session to jwt

--- routes/secureRoute/_router.js
+++ routes/secureRoute/_router.js
@@ -1,8 +1,9 @@
 const path = require('path');
 const { getRoutes } = require('../../utils/utils');
 
+const directoryPath = path.join(__dirname);
+
 module.exports = (app) => {
-  const directoryPath = path.join(__dirname);
   getRoutes(directoryPath).forEach(([route, routeObj]) => {
     app.use(route, routeObj);
   });
