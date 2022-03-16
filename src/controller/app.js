cb7f6a522c35dfebe5677f054ccf42b1645785b3
Konstantin Kozokar <konstantin@neotalogic.com>
2022-03-16 03:12:04 +0100

    [EDIT] Update build files

--- src/controller/app.js
+++ src/controller/app.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import path from "path";
+import { authenticate } from "../utils";
 
 const router = Router();
 
-router.get("/*", (req, res) => {
+router.get("/*", authenticate(), (req, res) => {
   res.sendFile(path.join(__dirname, "../../", "public", "index.html"));
 });
 
