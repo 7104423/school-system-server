8a91477abcc098ce28f7931ef2350083ed48e1e8
Konstantin Kozokar <konstantin@neotalogic.com>
2022-04-10 18:13:01 +0200

    main [FIX] Route

--- src/controller/app.js
+++ src/controller/app.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import path from "path";
-import { authenticate } from "../utils";
 
 const router = Router();
 
-router.get("/*", authenticate(), (req, res) => {
+router.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../../", "public", "index.html"));
 });
 
