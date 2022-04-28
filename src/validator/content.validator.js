424ccc42522b76e33d4cf4de1cd42d62bbbfd448
Jan <none>
2022-04-28 16:51:27 +0200

    [ADD] content validator

---
+++ src/validator/content.validator.js
@@ -0,0 +1,28 @@
+import { ajvUtils  } from "../utils/ajv.js";
+
+const validateCreateSchema = {
+    type: "object",
+    properties: {
+        type: {
+            type: "string",
+            maxLength: 200
+        },
+        content: {
+            type: "string",
+            maxLength: 16000
+        },
+        subject: {
+            type: "string",
+            maxLength: 200
+        },
+        topic: {
+            type: "string",
+            maxLength: 200
+        }
+    },
+    reguired: ["type"]
+};
+
+export const validateCreate = (data) => {
+    return ajvUtils.compile(validateCreateSchema)(data);
+};
