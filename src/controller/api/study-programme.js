10cef45403236e42fa50e09156520de025a18f38
Jan <none>
2022-05-08 17:06:40 +0200

    [EDIT] validations (JSON Schema)
    - added update schemas for validation
    - validations linked to controller/api routes
    - validations for studyProgramme, subject, topic and content use JSON
      schema

--- src/controller/api/study-programme.js
+++ src/controller/api/study-programme.js
@@ -2,12 +2,16 @@ import { Router } from "express";
 import { authenticate, availableFor } from "../../utils";
 import { Request, Response } from "express";
 import { StudyProgrammeDAO } from "./../../dao/studyProgramme.dao.js";
+import { validateCreate, validateUpdate } from "../../validator/studyProgramme.validator.js";
 
 const router = Router();
 
 router.post("/create", authenticate(), availableFor(["ADMIN"]), async (req, res) => {
 	let result;
 	try {
+		if (!validateCreate(req.body)) {
+			throw new Error("Validation failed");
+		}
 		result = await StudyProgrammeDAO.create(req.body)
 		res.json(result);
 	} catch (error) {
@@ -39,6 +43,9 @@ router.get( "/list", authenticate(), availableFor(["ADMIN", "STUDENT", "TEACHER"
 router.post("/update", authenticate(), availableFor(["ADMIN"]), async (req, res) => {
 	let result;
 	try {
+		if (!validateUpdate(req.body)) {
+			throw new Error("Validation failed");
+		}
 		result = await StudyProgrammeDAO.update(req.body.id, req.body);
 		res.json(result);
 	} catch (error) {
--- src/validator/subject.validator.js
+++ src/validator/subject.validator.js
@@ -1,4 +1,6 @@
-import { ajvUtils } from "../utils/ajv.js";
+//import { ajvUtils } from "../utils/ajv.js";
+import Ajv from "ajv";
+const ajvUtils = new Ajv();
 
 const validateCreateSchema = {
   type: "object",
@@ -32,9 +34,51 @@ const validateCreateSchema = {
       type: "string",
     },
   },
-  required: ["name", "supervisor", "teachers", "students", "studyProgramme"],
+  required: ["name", "supervisor", "teachers", "studyProgramme"],
+};
+
+const validateUpdateSchema = {
+  type: "object",
+  properties: {
+    id: {
+      type: "string",
+    },
+    name: {
+      type: "string",
+      minLength: 1,
+      maxLength: 255,
+    },
+    goal: {
+      type: "string",
+      minLength: 1,
+      maxLength: 4000,
+    },
+    supervisor: {
+      type: "string",
+    },
+    teachers: {
+      type: "array",
+      uniqueItems: true,
+      items: {
+        type: "string",
+      },
+    },
+    language: {
+      type: "string",
+      minLength: 1,
+      maxLength: 255,
+    },
+    studyProgramme: {
+      type: "string",
+    },
+  },
+  required: ["id", "name", "supervisor", "teachers", "studyProgramme"],
 };
 
 export const validateCreate = (data) => {
   return ajvUtils.compile(validateCreateSchema)(data);
+};
+
+export const validateUpdate = (data) => {
+  return ajvUtils.compile(validateUpdateSchema)(data);
 };
