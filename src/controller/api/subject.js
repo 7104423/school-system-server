00fe722ed647b6d5b14a5129c8f929939ac76021
Jan <none>
2022-05-08 15:13:00 +0200

    [EDIT] error messaging

--- src/controller/api/subject.js
+++ src/controller/api/subject.js
@@ -11,7 +11,7 @@ router.post( "/create", authenticate(), availableFor(["ADMIN"]), async ( req, re
     result = await SubjectDAO.create(req.body)
     res.json( result );
   } catch (error) {
-    res.status(500).json( { error } );
+    res.status(500).json( { message: error.message } );
   }
 } );
 
@@ -21,7 +21,7 @@ router.get( "/:id", authenticate(), availableFor(["ADMIN", "STUDENT", "TEACHER"]
     result = await SubjectDAO.get( req.params.id );
     res.json( result );
   } catch ( error ) {
-    res.status(500).json( { error } );
+    res.status(500).json( { message: error.message } );
   }
 } );
 
@@ -32,7 +32,7 @@ router.get( "/list", authenticate(), availableFor(["ADMIN", "STUDENT", "TEACHER"
     result = await SubjectDAO.list();
     res.json( result );
   } catch ( error ) {
-    res.status(500).json( { error } );
+    res.status(500).json( { message: error.message } );
   }
 } );
 
@@ -42,7 +42,7 @@ router.post( "/update", authenticate(), availableFor(["ADMIN", "TEACHER"]), asyn
     result = await SubjectDAO.update( req.body.id, req.body );
     res.json( result );
   } catch ( error ) {
-    res.status(500).json( { error } );
+    res.status(500).json( { message: error.message } );
   }
 } );
 
@@ -52,7 +52,7 @@ router.post( "/delete", authenticate(), availableFor(["ADMIN"]), async ( req, re
     result = await SubjectDAO.delete(req.body.id);
     res.json( result );
   } catch ( error ) {
-    res.status(500).json( { error } );
+    res.status(500).json( { message: error.message } );
   }
 } );
 
