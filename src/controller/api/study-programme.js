7690785b12e7843ffcac9abcff10266b42742671
Konstantin Kozokar <konstantin@neotalogic.com>
2022-05-10 17:18:13 +0200

    Revert "main [FIX] Route list"
    This reverts commit 2ea014bdefe7f0f0d0634379dbd51fde18322add.

--- src/controller/api/study-programme.js
+++ src/controller/api/study-programme.js
@@ -5,79 +5,55 @@ import { StudyProgrammeDAO } from "./../../dao/studyProgramme.dao.js";
 
 const router = Router();
 
-router.post(
-  "/create",
-  authenticate(),
-  availableFor(["ADMIN"]),
-  async (req, res) => {
-    let result;
-    try {
-      result = await StudyProgrammeDAO.create(req.body);
-      res.json(result);
-    } catch (error) {
-      res.status(500).json({ error });
-    }
-  }
-);
+router.post("/create", authenticate(), availableFor(["ADMIN"]), async (req, res) => {
+	let result;
+	try {
+		result = await StudyProgrammeDAO.create(req.body)
+		res.json(result);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+});
 
-router.get(
-  "/list",
-  authenticate(),
-  availableFor(["ADMIN", "STUDENT", "TEACHER"]),
-  async (req, res) => {
-    let result;
-    try {
-      result = await StudyProgrammeDAO.list();
-      res.json(result);
-    } catch (error) {
-      res.status(500).json({ error });
-    }
-  }
-);
+router.get("/:id", authenticate(), availableFor(["ADMIN", "STUDENT", "TEACHER"]), async (req, res) => {
+	let result;
+	try {
+		result = await StudyProgrammeDAO.get(req.params.id);
+		res.json(result);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+});
 
-router.get(
-  "/:id",
-  authenticate(),
-  availableFor(["ADMIN", "STUDENT", "TEACHER"]),
-  async (req, res) => {
-    let result;
-    try {
-      result = await StudyProgrammeDAO.get(req.params.id);
-      res.json(result);
-    } catch (error) {
-      res.status(500).json({ error });
-    }
-  }
-);
+// @TODO: fix this, this route doesn't work...
+router.get( "/list", authenticate(), availableFor(["ADMIN", "STUDENT", "TEACHER"]), async ( req, res ) => {
+	let result;
+	try {
+		result = await StudyProgrammeDAO.list();
+		res.json( result );
+	} catch ( error ) {
+		res.status(500).json( { error } );
+	}
+} );
 
-router.post(
-  "/update",
-  authenticate(),
-  availableFor(["ADMIN"]),
-  async (req, res) => {
-    let result;
-    try {
-      result = await StudyProgrammeDAO.update(req.body.id, req.body);
-      res.json(result);
-    } catch (error) {
-      res.status(500).json({ error });
-    }
-  }
-);
+router.post("/update", authenticate(), availableFor(["ADMIN"]), async (req, res) => {
+	let result;
+	try {
+		result = await StudyProgrammeDAO.update(req.body.id, req.body);
+		res.json(result);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+});
 
-router.post(
-  "/delete",
-  authenticate(),
-  availableFor(["ADMIN"]),
-  async (req, res) => {
-    let result;
-    try {
-      result = await StudyProgrammeDAO.delete(req.body.id);
-      res.json(result);
-    } catch (error) {
-      res.status(500).json({ error });
-    }
-  }
-);
+router.post("/delete", authenticate(), availableFor(["ADMIN"]), async (req, res) => {
+	let result;
+	try {
+		result = await StudyProgrammeDAO.delete(req.body.id);
+		res.json(result);
+	} catch (error) {
+		res.status(500).json({ error });
+	}
+});
 
 export default router;
