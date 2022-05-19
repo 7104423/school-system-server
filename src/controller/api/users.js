11437e6d7848bc77b13da7f98bf6c926cde4ec35
Jan <none>
2022-05-19 22:44:18 +0200

    [REMOVE] removed unused users route

--- src/controller/api/users.js
+++ src/controller/api/users.js
@@ -44,19 +44,6 @@ router.get(
   }
 );
 
-router.get(
-  "/",
-  authenticate(),
-  availableFor(["STUDENT"]),
-  /**
-   * @param {Request} req
-   * @param {Response} res
-   */
-  (req, res) => {
-    res.json({ message: "This section is available only for students" });
-  }
-);
-
 // update
 // @TODO
 
