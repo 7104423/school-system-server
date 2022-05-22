99fc2c715a7d5b8e5fd0b14491f23e72b5c776af
Konstantin Kozokar <konstantin.kozokar@gmail.com>
2022-05-22 02:05:55 +0200

    digital content create abl changes

--- src/abl/content/create.abl.js
+++ src/abl/content/create.abl.js
@@ -1,15 +1,27 @@
 import { ContentDAO } from "../../dao/content.dao.js";
+import { TopicDAO } from "../../dao/topic.dao.js";
 import { validateCreate } from "../../validator/content.validator.js";
 
 export async function CreateAbl(req, res) {
-	let result;
-	try {
-		if (!validateCreate(req.body)) {
-			throw new Error("Validation failed");
-		}
-		result = await ContentDAO.create(req.body);
-		res.json(result);
-	} catch (error) {
-		res.status(500).json({ message: error.message });
-	}
+  let result;
+  try {
+    if (!validateCreate(req.body)) {
+      throw new Error("Validation failed");
+    }
+
+    const { topic, subject } = req.body;
+    const topicDao = await TopicDAO.get(topic);
+    if (topicDao.subject !== subject) {
+      res
+        .status(400)
+        .json({
+          message: "Topic's subject is different from Digital Content subject",
+        });
+    }
+
+    result = await ContentDAO.create(req.body);
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 }
